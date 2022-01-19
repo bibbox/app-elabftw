ed3145810ef2ac4fdf960cf827e851d4c9ef22b4
Emilian <emilian.jung+github@gmail.com>
2022-01-19 17:26:52 +0100

    minor update

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ docker network create bibbox-default-network
 
 docker-compose up
 
-docker exec -it elabftw bin/install start
+docker exec -it bibbox-web bin/install start
+
+#docker exec -it bibbox-mysql mysql -u elabftw --database="elabftw" --password="WnN8naVZZ4b8TrfAMSmyYeoz5vjbPTg" --execute='UPDATE config SET conf_value = "http://localhost:8080" WHERE conf_name = "url";'
+
+#docker exec -it bibbox-web sed -i 's/session.cookie_secure = true/session.cookie_secure = false' ../etc/php8/php.ini
 
