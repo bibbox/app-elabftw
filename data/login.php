5c686c3a12d4690ff6fde60b31b261ad8b4575de
AlexMartazan <118280106+AlexMartazan@users.noreply.github.com>
2023-07-13 10:30:03 +0200

    changed login.php

--- data/login.php
+++ data/login.php
@@ -1,4 +1,7 @@
-* @copyright 2012 Nicolas CARPi
+<?php declare(strict_types=1);
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2012 Nicolas CARPi
  * @see https://www.elabftw.net Official website
  * @license AGPL-3.0
  * @package elabftw
